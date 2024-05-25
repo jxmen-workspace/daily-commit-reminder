c4f62257e1bed2f87608a097b5c1ea4725decf45
jxmen <sprnd645@gmail.com>
2024-05-25 22:44:07 +0900

    fix: 미사용 테스트 코드 제거

--- app/src/test/kotlin/org/example/github/dto/GitHubPublicEventOfaUserTest.kt
+++ app/src/test/kotlin/org/example/github/dto/GitHubPublicEventOfaUserTest.kt
@@ -5,50 +5,6 @@ import io.kotest.matchers.shouldBe
 import java.time.LocalDateTime
 
 class GitHubPublicEventOfaUserTest : DescribeSpec({
-    describe("todayCommitEvent") {
-        context("날짜가 같고 commit 종류의 이벤트라면") {
-            it("true를 반환한다") {
-
-                val gitHubPublicEventOfaUser =
-                    GitHubPublicEventOfaUser(
-                        id = "1",
-                        type = "PushEvent",
-                        createdAt = LocalDateTime.of(2024, 5, 21, 0, 0, 0),
-                    )
-
-                gitHubPublicEventOfaUser.isTodayCommitEvent(LocalDateTime.of(2024, 5, 21, 0, 0, 0)) shouldBe true
-            }
-        }
-
-        context("날짜가 다르다면") {
-            it("false를 반환한다") {
-                val gitHubPublicEventOfaUser =
-                    GitHubPublicEventOfaUser(
-                        id = "1",
-                        type = "PushEvent",
-                        createdAt =
-                            LocalDateTime.of(2024, 5, 21, 0, 0, 0),
-                    )
-
-                gitHubPublicEventOfaUser.isTodayCommitEvent(LocalDateTime.of(2024, 5, 22, 0, 0, 0)) shouldBe false
-            }
-        }
-
-        context("commit이 아닌 종류의 이벤트라면") {
-            it("false를 반환한다") {
-                val gitHubPublicEventOfaUser =
-                    GitHubPublicEventOfaUser(
-                        id = "1",
-                        type = "WatchEvent",
-                        createdAt =
-                            LocalDateTime.of(2024, 5, 21, 0, 0, 0),
-                    )
-
-                gitHubPublicEventOfaUser.isTodayCommitEvent(LocalDateTime.of(2024, 5, 21, 0, 0, 0)) shouldBe false
-            }
-        }
-    }
-
     describe("hasSameCommitSha") {
         context("commit sha가 같다면") {
             it("true를 반환한다") {
